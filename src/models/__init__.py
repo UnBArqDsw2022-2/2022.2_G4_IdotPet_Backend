1e94b213580f701035ca7958bd28f3214ec47bba
Thalisson-Alves <thalisson2030@gmail.com>
2023-01-19 22:09:41 -0300

    Unify models related to user

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,1 @@
-from .user_model import UserModel
-from .base_user_model import BaseUserModel
-from .ong_user_model import OngUserModel
+from .user_model import UserModel, BaseUserModel, OngUserModel
--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,6 +1,25 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import (Column, Date, DateTime, ForeignKey, Integer,
+                        LargeBinary, String)
+from sqlalchemy.sql import func
 
-from .base_user_model import BaseUserModel
+from utils.database import Base
+
+
+class BaseUserModel(Base):
+    __tablename__ = 'base_users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_type = Column(String(32), nullable=False)
+    image = Column(LargeBinary, nullable=True)
+    name = Column(String(150), nullable=False)
+    email = Column(String(150), unique=True, nullable=False)
+    password = Column(String(200), nullable=False)
+    birth_day = Column(Date, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(),
+                        onupdate=func.now())
+
+    __mapper_args__ = {'polymorphic_on': user_type}
 
 
 class UserModel(BaseUserModel):
@@ -10,3 +29,13 @@ class UserModel(BaseUserModel):
     cpf = Column(String(11), unique=True, nullable=False)
 
     __mapper_args__ = {'polymorphic_identity': 'user'}
+
+
+class OngUserModel(BaseUserModel):
+    __tablename__ = 'ong_users'
+
+    id = Column(Integer, ForeignKey('base_users.id'), primary_key=True)
+    cnpj = Column(String(14), unique=True, nullable=False)
+    pix = Column(String(512))
+
+    __mapper_args__ = {'polymorphic_identity': 'ong'}
