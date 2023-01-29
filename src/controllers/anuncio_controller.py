26959c634b14c073e9a43440c3e054929dd199f4
Thalisson-Alves <thalisson2030@gmail.com>
2023-01-29 11:37:19 -0300

    Validação da criação de Anúncios
    Co-authored-by: Pedro Cassiano <pcassianomartins@gmail.com>

--- src/controllers/anuncio_controller.py
+++ src/controllers/anuncio_controller.py
@@ -1,30 +1,40 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Response
-from sqlalchemy.exc import IntegrityError
+from fastapi import APIRouter, Depends, HTTPException, status
 
-from models import AnuncioModel, BaseUserModel
+from models import AnuncioModel, PetModel
 from repositories import repository_factory
 from schemes.anuncio_schemes import AnuncioCreate, AnuncioUpdate, AnuncioView
 from utils.database import AsyncSession, get_db
-from utils.security import logged_user
+from utils.security import decode_token
 
 router = APIRouter(prefix='/anuncio', tags=['Anúncio'])
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=AnuncioView)
-async def create(anuncio: AnuncioCreate, current_user: BaseUserModel = Depends(logged_user), db: AsyncSession = Depends(get_db)):
-    try:
-        anuncio_model = AnuncioModel(**{**anuncio.dict(exclude_unset=True), 'user_id': current_user.id})
-        repository = await repository_factory(AnuncioModel, db)
-        await repository.create(anuncio_model)
-        return anuncio_model
-    except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail='email and cpf must be unique',
-        )
+async def create(anuncio: AnuncioCreate, logger_user_id: int = Depends(decode_token), db: AsyncSession = Depends(get_db)):
+    if not await is_pet_from_user(anuncio.pet_id, logger_user_id, db):
+        raise HTTPException(status.HTTP_404_NOT_FOUND, 'Pet não encontrado')
 
-@router.patch('/{id}', status_code=status.HTTP_200_OK, response_model=AnuncioView)
-async def patch(anuncio: AnuncioUpdate, id: int, _: BaseUserModel = Depends(logged_user), db: AsyncSession = Depends(get_db)):
+    anuncio_model = AnuncioModel(
+        **{**anuncio.dict(exclude_unset=True), 'user_id': logger_user_id}
+    )
+    repository = await repository_factory(AnuncioModel, db)
+    await repository.create(anuncio_model)
+    return anuncio_model
+
+
+async def is_pet_from_user(pet_id: int, user_id: int, db: AsyncSession) -> bool:
+    repository = await repository_factory(PetModel, db)
+    return bool(await repository.get_all({'id': pet_id, 'user_id': user_id}))
+
+
+@router.get('/', status_code=status.HTTP_201_CREATED, response_model=AnuncioView, dependencies=[Depends(decode_token)])
+async def anuncio_get_all(db: AsyncSession = Depends(get_db)):
+    repository = await repository_factory(AnuncioModel, db)
+    return await repository.get_all()
+
+
+@router.patch('/{id}', status_code=status.HTTP_200_OK, response_model=AnuncioView, dependencies=[Depends(decode_token)])
+async def patch(anuncio: AnuncioUpdate, id: int, db: AsyncSession = Depends(get_db)):
     repository = await repository_factory(AnuncioModel, db)
     anuncio_model = await repository.get_by_id(id)
     for key, value in anuncio.__dict__.items():
@@ -36,8 +46,8 @@ async def patch(anuncio: AnuncioUpdate, id: int, _: BaseUserModel = Depends(logg
     return await repository.update(anuncio_model)
 
 
-@router.put('/{id}', status_code=status.HTTP_200_OK, response_model=AnuncioView)
-async def update(anuncio: AnuncioUpdate, id: int, _: BaseUserModel = Depends(logged_user), db: AsyncSession = Depends(get_db)):
+@router.put('/{id}', status_code=status.HTTP_200_OK, response_model=AnuncioView, dependencies=[Depends(decode_token)])
+async def update(anuncio: AnuncioUpdate, id: int, db: AsyncSession = Depends(get_db)):
     repository = await repository_factory(AnuncioModel, db)
     anuncio_model = await repository.get_by_id(id)
     for key, value in anuncio.__dict__.items():
@@ -46,19 +56,14 @@ async def update(anuncio: AnuncioUpdate, id: int, _: BaseUserModel = Depends(log
     return await repository.update(anuncio_model)
 
 
-@router.delete('/{id}', status_code=status.HTTP_200_OK, response_model=AnuncioView)
-async def delete(id: int, _: BaseUserModel = Depends(logged_user), db: AsyncSession = Depends(get_db)):
+@router.delete('/{id}', status_code=status.HTTP_200_OK, response_model=AnuncioView, dependencies=[Depends(decode_token)])
+async def delete(id: int, db: AsyncSession = Depends(get_db)):
     repository = await repository_factory(AnuncioModel, db)
     anuncio = await repository.get_by_id(id)
     return await repository.delete(anuncio)
 
 
-@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=AnuncioView, dependencies=[Depends(logged_user)])
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=AnuncioView, dependencies=[Depends(decode_token)])
 async def get_by_id(id: int, db: AsyncSession = Depends(get_db)):
     repository = await repository_factory(AnuncioModel, db)
     return await repository.get_by_id(id)
-
-@router.get('/', status_code=status.HTTP_201_CREATED, response_model=AnuncioView)
-async def anuncio_get_all(db: AsyncSession = Depends(get_db)):
-    repository = await repository_factory(AnuncioModel, db)
-    return await repository.get_all()
